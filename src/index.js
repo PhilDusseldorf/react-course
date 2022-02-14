b19557ec3b60d21c83ddadbde0e492280d070365
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2022-02-14 08:56:03 +0100

    start objecitve 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+ReactDOM.render(<App />, document.getElementById('root'));
+
+/* Objective 1:
+Render an unordered List to the screen. The list should contain at least three items
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <ul>
+    <li>item 1</li>
+    <li>item 2</li>
+    <li>item 3</li>
+  </ul>,
+  document.getElementById('root'));
+*/
