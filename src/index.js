835668aa6274d38be827990b6a16f46874e57935
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2022-02-14 11:49:21 +0100

    finished objective 2

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 /* Objective 1:
 Render an unordered List to the screen. The list should contain at least three items
+
 ReactDOM.render(
   <ul>
     <li>item 1</li>
@@ -13,4 +14,30 @@ ReactDOM.render(
     <li>item 3</li>
   </ul>,
   document.getElementById('root'));
+*/
+
+/* Objective 2:
+Create a functional component called MyInfo that returns the following UI:
+  a. An h1 with your name
+  b. A paragraph with a little blurb about yourself
+  c. An ordered or unordered list of your top 3 vacation spots
+Render that to the browser!
+
+function MyInfo() {
+  return (
+    <>
+    <h1>Phil</h1>
+    <p>
+    I am learning JAVA for the OCA exam and now trying to learn react.js for my internship that is upcoming.
+    </p>
+    <ul>
+    <li>Shanghai</li>
+    <li>New York</li>
+    <li>Mykonos</li>
+    </ul>
+    </>
+    );
+  }
+
+  ReactDOM.render(<MyInfo />, document.getElementById('root'));
 */
