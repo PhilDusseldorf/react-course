9bd22eb5fe01955d806320f4f28291030077ed01
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2022-02-18 13:42:56 +0100

    styled Joke

--- src/objectives/Joke.js
+++ src/objectives/Joke.js
@@ -4,10 +4,10 @@ function Joke(params) {
 
     return (
         <div style={{ fontFamily: "Arial, Helvetica, sans-serif" }}>
-            <p style={{ display: !params.question && "none" }}>
+            <p style={{ display: !params.question && "none", color: "grey" }}>
                 Question: {params.question}
             </p>
-            <p style={{ textIndent: params.question && "20px" }}>
+            <p style={{ textIndent: params.question && "20px", color: !params.question && "grey" }}>
                 Punchline: {params.punchline}
             </p>
         </div >
