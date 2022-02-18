2c26407f0f17c6241c0563f8280f769fac5e1e15
PhilDusseldorf <86970605+PhilDusseldorf@users.noreply.github.com>
2022-02-18 16:29:33 +0100

    finished Todo Task 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import TodoItem from "./components/TodoItem";
+import todoData from "./components/todoData.js";
 
 function App() {
     /*Todo App Part 1
@@ -30,16 +31,33 @@ function App() {
  
     Also I have to create the style of the page 
     either using a CSS file or inline styles or both.
+ 
+        const styles = {
+        borderBottom: '1px solid rgba(0, 0, 0, 0.05)'
+    };
+ 
+    return (
+        <div style={styles}>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
+    );
+}
+ 
+export default App;
     */
+
+    // import the data from the todoData
+    const todoArray = todoData.map(data => <TodoItem key={data.id} item={data} />);
+
     const styles = {
         borderBottom: '1px solid rgba(0, 0, 0, 0.05)'
     };
 
     return (
         <div style={styles}>
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todoArray}
         </div>
     );
 }
